6cd2dd724042caaffcbe2888420f781e6bb93fe3
nikita_bautin <alliancesuck13@yandex.ru>
2024-04-15 01:47:29 +0700

    1. прикрутил слайдер 2. исправил/добавил кое-что по-мелочи

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let buttonOpen = document.querySelector(".b-block__button-open");
 let moreLink = document.querySelector(".section-block__link");
 let sectionBlockText = document.querySelector(".section-block__text");
 let brandsList = document.querySelector(".brands__list");
-let buttonShowBrands = document.querySelector('.show-brands__button');
+let buttonShowBrands = document.querySelector(".show-brands__button");
 
 buttonClose.addEventListener("click", (evt) => {
   asideMenu.classList.add("aside-menu--closed");
@@ -20,20 +20,37 @@ buttonOpen.addEventListener("click", (evt) => {
   overlay.classList.remove("visually-hidden");
 });
 
+overlay.addEventListener("click", (evt) => {
+  asideMenu.classList.add("aside-menu--closed");
+  overlay.classList.add("visually-hidden");
+});
+
 moreLink.addEventListener("click", (evt) => {
   evt.preventDefault();
   sectionBlockText.classList.toggle("section-block__text--show");
   moreLink.textContent = "Скрыть";
-  if (!document.querySelector('.section-block__text--show')) {
+  if (!document.querySelector(".section-block__text--show")) {
     moreLink.textContent = "Читать далее";
   }
 });
 
 buttonShowBrands.addEventListener("click", (evt) => {
   evt.preventDefault();
-  brandsList.classList.toggle('brands__list--show');
+  brandsList.classList.toggle("brands__list--show");
   buttonShowBrands.textContent = "Скрыть";
-  if (!document.querySelector('.brands__list--show')) {
+  if (!document.querySelector(".brands__list--show")) {
     buttonShowBrands.textContent = "Показать все";
   }
-})
+});
+
+if (window.innerWidth >= 320 && window.innerWidth <= 360) {
+  asideMenu.style.width = window.innerWidth + "px";
+}
+
+new Swiper(".swiper", {
+  spaceBetween: 200,
+  slidesPerView: 2,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
