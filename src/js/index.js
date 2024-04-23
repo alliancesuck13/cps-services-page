7e6228fc936a9499221af39b4cdba15f10a84140
Nikita Bautin <alliancesuck13@yandex.ru>
2024-04-23 12:40:05 +0700

    Переделал полностью scss структуру
    Переделал структуру scss
    Пофиксил баги

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,116 @@
 import { initSwiper } from './slider/swiper.js'
 
-import { makeEventOpen, makeEventClose, makeEventShowAndHide, makeEventChangeWidthMenu } from './events/makeEvent.js'
+import {
+  makeEventOpen,
+  makeEventClose,
+  makeEventShowAndHide,
+  makeEventChangeWidthMenu
+} from './events/makeEvent.js'
 
-import { buttonClose, buttonOpen, asideMenu, overlay, moreLink, sectionBlockText, brandsList, buttonShowBrands, buttonShowRepair, repairList, buttonCloseModal, modalFeedback, buttonOpenFeedback, buttonOpenCall, modalCall } from './events/vars.js'
+import {
+  buttonClose,
+  buttonOpen,
+  asideMenu,
+  overlay,
+  moreLink,
+  sectionBlockText,
+  brandsList,
+  buttonShowBrands,
+  buttonShowRepair,
+  repairList,
+  buttonCloseModal,
+  modalFeedback,
+  buttonOpenFeedback,
+  buttonOpenCall,
+  modalCall,
+  modal
+} from './events/vars.js'
 
 import '../scss/style.scss'
 
-makeEventClose(buttonClose, asideMenu, 'aside-menu--closed', overlay, 'visually-hidden');
-makeEventClose(overlay, asideMenu, 'aside-menu--closed', overlay, 'visually-hidden');
-makeEventClose(buttonCloseModal, modalFeedback, 'modal--hide', overlay, 'visually-hidden');
-makeEventClose(overlay, modalFeedback, 'modal--hide', overlay, 'visually-hidden');
-makeEventClose(buttonCloseModal, modalCall, 'modal--hide', overlay, 'visually-hidden');
-makeEventClose(overlay, modalCall, 'modal--hide', overlay, 'visually-hidden');
+makeEventClose(
+  buttonClose,
+  asideMenu,
+  'aside-menu--closed',
+  overlay,
+  'visually-hidden'
+)
+makeEventClose(
+  overlay,
+  asideMenu,
+  'aside-menu--closed',
+  overlay,
+  'visually-hidden'
+)
+makeEventClose(
+  buttonCloseModal,
+  modalFeedback,
+  'modal--hide',
+  overlay,
+  'visually-hidden'
+)
+makeEventClose(
+  overlay,
+  modalFeedback,
+  'modal--hide',
+  overlay,
+  'visually-hidden'
+)
+makeEventClose(
+  buttonCloseModal,
+  modalCall,
+  'modal--hide',
+  overlay,
+  'visually-hidden'
+)
+makeEventClose(overlay, modalCall, 'modal--hide', overlay, 'visually-hidden')
 
-makeEventOpen(buttonOpen, asideMenu, 'aside-menu--closed', overlay, 'visually-hidden');
-makeEventOpen(buttonOpenFeedback, modalFeedback, 'modal--hide', overlay, 'visually-hidden');
-makeEventOpen(buttonOpenCall, modalCall, 'modal--hide', overlay, 'visually-hidden');
+makeEventOpen(
+  buttonOpen,
+  asideMenu,
+  'aside-menu--closed',
+  overlay,
+  'visually-hidden'
+)
+makeEventOpen(
+  buttonOpenFeedback,
+  modalFeedback,
+  'modal--hide',
+  overlay,
+  'visually-hidden'
+)
+makeEventOpen(
+  buttonOpenCall,
+  modalCall,
+  'modal--hide',
+  overlay,
+  'visually-hidden'
+)
 
-makeEventShowAndHide(moreLink, sectionBlockText, 'section-block__text--show', 'Скрыть', 'Читать далее');
-makeEventShowAndHide(buttonShowBrands, brandsList, 'brands__list--show', 'Скрыть', 'Показать все');
-makeEventShowAndHide(buttonShowRepair, repairList, 'repair__list--show', 'Скрыть', 'Показать все');
+makeEventShowAndHide(
+  moreLink,
+  sectionBlockText,
+  'section-block__text--show',
+  'Скрыть',
+  'Читать далее'
+)
+makeEventShowAndHide(
+  buttonShowBrands,
+  brandsList,
+  'brands__list--show',
+  'Скрыть',
+  'Показать все'
+)
+makeEventShowAndHide(
+  buttonShowRepair,
+  repairList,
+  'repair__list--show',
+  'Скрыть',
+  'Показать все'
+)
 
-makeEventChangeWidthMenu(asideMenu);
+makeEventChangeWidthMenu(asideMenu)
+makeEventChangeWidthMenu(modalFeedback)
+makeEventChangeWidthMenu(modalCall)
 
-initSwiper();
+initSwiper()
